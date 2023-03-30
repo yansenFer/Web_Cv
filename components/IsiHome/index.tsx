5d76b8669d5f1a7d5dc0fc9471371d7ca7a37888
lnc23 <yansenferdinand6@gmail.com>
2023-03-31 06:55:49 +0700

    animation in out

--- components/IsiHome/index.tsx
+++ components/IsiHome/index.tsx
@@ -1,42 +1,51 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 interface booleanProps {
   isAnimated: boolean
 }
 
 const IsiHome = ({ isAnimated }: booleanProps) => {
-  const [hidden, setHidden] = useState("")
+  const [isHidden, setIsHidden] = useState(false)
+  useEffect(() => {
+    if (!isAnimated) {
+      const timeout = setTimeout(() => {
+        setIsHidden(true)
+      }, 1000)
 
-  const hiddenBoolan = () => {
-    setTimeout(() => {
-      setHidden("hidden")
-    }, 1000)
-  }
-
-  console.log(hidden)
+      return () => {
+        clearTimeout(timeout)
+      }
+    } else {
+      setIsHidden(false)
+    }
+  }, [isAnimated])
 
   return (
-    <div
-      className={`mt-10 h-screen w-full bg-[#0A2647] ${
-        isAnimated
-          ? "animate__animated animate__fadeInUp animate__fast animate__delay-1s"
-          : `animate__animated animate__fadeOutUp animate__fast hidden ${hiddenBoolan()} ${hidden}`
-      } h-max-screen overflow-y-scroll rounded-[40px]`}
-    >
-      <div className="flex flex-col w-full h-full justify-center items-center">
-        <span className="font-[600] text-[30px] text-zinc-200">
-          Yanson Ferdinand Kurniadi
-        </span>
-        <div className="flex flex-row gap-2 self-center justify-center">
-          <span className="font-[600] text-[24px] animate__animated animate__bounce animate__delay-3s text-[#57C5B6]">
-            Frontend
-          </span>
-          <span className="font-[600] text-[24px] animate__animated animate__bounce animate__delay-3s text-zinc-200">
-            Developer
-          </span>
+    <>
+      {isHidden ? null : (
+        <div
+          className={`mt-10 h-screen w-full bg-[#0A2647] ${
+            isAnimated
+              ? "animate__animated animate__fadeInUp animate__fast animate__delay-1s"
+              : `animate__animated animate__fadeOutUp animate__fast`
+          } h-max-screen overflow-y-scroll rounded-[40px]`}
+        >
+          <div className="flex flex-col w-full h-full justify-center items-center">
+            <span className="font-[600] text-[30px] text-zinc-200">
+              Yanson Ferdinand Kurniadi
+            </span>
+            <div className="flex flex-row gap-2 self-center justify-center">
+              <span className="font-[600] text-[24px] animate__animated animate__bounce animate__delay-3s text-[#57C5B6]">
+                Frontend
+              </span>
+              <span className="font-[600] text-[24px] animate__animated animate__bounce animate__delay-3s text-zinc-200">
+                Developer
+              </span>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
 
